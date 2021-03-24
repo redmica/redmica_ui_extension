a37ff8efa50edb5f0963c1fde16b9d1e11738581
Takenori Takaki <takenory@gmail.com>
2021-03-24 10:45:48 +0900

    Add settings to display/hide burndown chart

--- test/system/burndown_chart_test.rb
+++ test/system/burndown_chart_test.rb
@@ -8,23 +8,37 @@ class BurndownChartTest < ApplicationSystemTestCase
            :trackers, :projects_trackers, :enumerations, :issue_statuses, :issues
 
   def test_render_chart_on_version_detail
-    log_user('jsmith', 'jsmith')
-    visit '/versions/2'
+    with_settings :plugin_redmica_ui_extension => {'burndown_chart' => {'enabled' => 1}} do
+      log_user('jsmith', 'jsmith')
+      visit '/versions/2'
 
-    assert page.has_css?('#version-detail-chart')
-    within '#version-detail-chart' do
-      assert page.find('#burndown-chart')
+      assert page.has_css?('#version-detail-chart')
+      within '#version-detail-chart' do
+        assert page.find('#burndown-chart')
+      end
     end
   end
 
   def test_does_not_render_chart_if_issues_empty
-    version = Version.find(2)
-    version.fixed_issues.delete_all
+    with_settings :plugin_redmica_ui_extension => {'burndown_chart' => {'enabled' => 1}} do
+      version = Version.find(2)
+      version.fixed_issues.delete_all
 
-    log_user('jsmith', 'jsmith')
-    visit '/versions/2'
+      log_user('jsmith', 'jsmith')
+      visit '/versions/2'
 
-    assert page.has_no_css?('#version-detail-chart')
-    assert page.all(:css, '#version-detail-chart').empty?
+      assert page.has_no_css?('#version-detail-chart')
+      assert page.all(:css, '#version-detail-chart').empty?
+    end
+  end
+
+  def test_does_not_render_chart_if_setting_enabled_is_nil
+    with_settings :plugin_redmica_ui_extension => {'burndown_chart' => {'enabled' => nil}} do
+      log_user('jsmith', 'jsmith')
+      visit '/versions/2'
+
+      assert page.has_no_css?('#version-detail-chart')
+      assert page.all(:css, '#version-detail-chart').empty?
+    end
   end
 end
