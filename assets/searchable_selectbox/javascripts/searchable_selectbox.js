3048073cc99f5792912ece60ef8d1788a9b1290d
ishikawa999 <remember929@gmail.com>
2021-06-11 04:53:28 +0000

    Follow the change of redmine_searchable_selectbox

--- assets/searchable_selectbox/javascripts/searchable_selectbox.js
+++ assets/searchable_selectbox/javascripts/searchable_selectbox.js
@@ -1,13 +1,13 @@
 // Replace with select2 when the HTTP status of ajax request is a success.
 // (by pure jquery)
-$(document).ajaxSuccess(function () {
+$(document).ajaxSuccess(function() {
   replaceSelect2();
   initAssignToMeLink();
 });
 
 // Replace with select2 when the HTTP status of data-remote request is a success.
 // (by rails-ujs)
-$(document).on('ajax:success', function () {
+$(document).on('ajax:success', function() {
   replaceSelect2();
   initAssignToMeLink();
 });
@@ -22,7 +22,7 @@ $(function() {
   if ($.ui && $.ui.dialog && $.ui.dialog.prototype._allowInteraction) {
     var ui_dialog_interaction = $.ui.dialog.prototype._allowInteraction;
     $.ui.dialog.prototype._allowInteraction = function(e) {
-      if ($( e.target ).closest('.select2-dropdown').length) { return true; }
+      if ($(e.target).closest('.select2-dropdown').length) { return true; }
       return ui_dialog_interaction.apply(this, arguments);
     };
   };
@@ -58,23 +58,31 @@ function replaceSelect2() {
     if (selectInTabular.length) {
       selectInTabular.select2({
         width: 'style'
-      }).on('select2:select', function () {
-        Rails.fire($(this)[0], 'change')
+      }).on('select2:select', function() {
+        // Rails.fire cannot be used in Redmine 3.x or earlier, so it will not be executed.
+        if (typeof Rails != 'undefined') { Rails.fire($(this)[0], 'change') }
       });
     }
 
     var other = $('select:not([multiple]):not([data-remote]):not(.select2-hidden-accessible)');
-    if (other.length) { other.select2().on('select2:select', function(){Rails.fire($(this)[0], 'change')}); }
+    if (other.length) {
+      other.select2().on('select2:select', function() {
+        // Rails.fire cannot be used in Redmine 3.x or earlier, so it will not be executed.
+        if (typeof Rails != 'undefined') { Rails.fire($(this)[0], 'change') }
+      });
+    }
 
     var excludedSelect = $('table.list td>select');
-    if (excludedSelect.length) { excludedSelect.select2('destroy'); }
+    if (excludedSelect.length) {
+      excludedSelect.select2('destroy');
+    }
   }
 }
 
 // Changed for a change event to occur when change a value in #issue_assigned_to_id.
 // https://github.com/ishikawa999/redmine_searchable_selectbox/issues/6
 function initAssignToMeLink() {
-  $('form#issue-form .assign-to-me-link').click(function (event) {
+  $('form#issue-form .assign-to-me-link').click(function(event) {
     event.preventDefault();
     var element = $(event.target);
     $('#issue_assigned_to_id').val(element.data('id')).change();
