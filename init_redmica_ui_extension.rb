d53bf2558ba0ad803a83b05e0c2404197ad46220
ishikawa999 <remember929@gmail.com>
2021-10-06 02:26:04 +0000

    Fix according to the warning when loading files

--- init_redmica_ui_extension.rb
+++ init_redmica_ui_extension.rb
@@ -1,21 +1,17 @@
 # frozen_string_literal: true
 
-# searchable_selectbox
-require_dependency 'searchable_selectbox/hook_listener'
-require_dependency 'searchable_selectbox/my_helper_patch'
-Rails.configuration.to_prepare do
-  MyHelper.include SearchableSelectbox::MyHelperPatch
-end
-
-# burndown_chart
-require_dependency 'burndown_chart/hook_listener'
-require_dependency 'burndown_chart/versions_helper_patch'
-Rails.configuration.to_prepare do
-  VersionsHelper.include BurndownChart::VersionsHelperPatch
-end
-
-# Common methods for redmica_ui_extension
-require_dependency 'redmica_ui_extension/setting_patch'
-Rails.configuration.to_prepare do
+Rails.application.reloader.to_prepare do
+  # Common methods for redmica_ui_extension
+  require 'redmica_ui_extension/setting_patch'
   Setting.include RedmicaUiExtension::SettingPatch
+
+  # searchable_selectbox
+  require 'searchable_selectbox/hook_listener'
+  require 'searchable_selectbox/my_helper_patch'
+  MyHelper.include SearchableSelectbox::MyHelperPatch
+
+  # burndown_chart
+  require 'burndown_chart/hook_listener'
+  require 'burndown_chart/versions_helper_patch'
+  VersionsHelper.include BurndownChart::VersionsHelperPatch
 end
