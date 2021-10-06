0e81fa9951a61341182a74832e7b19f712b9f1fc
ishikawa999 <remember929@gmail.com>
2021-10-06 02:39:19 +0000

    Add mermaid macro

---
+++ assets/mermaid_macro/javascripts/mermaid_macro.js
@@ -0,0 +1,38 @@
+// The conversion process when drawing the page is performed by initAllMermaidMacro().
+// After that, initMermaidMacro() is executed separately to support mermaid macro added by ajax, etc.
+
+$(function() {
+  initAllMermaidMacro();
+});
+
+function initAllMermaidMacro() {
+  if (ShowErrorMessageIfIE()) { return false; }
+
+  // Hide the target before the change by before-init-mermaid class until mermaid.initialize() or mermaid.init() is executed.
+  // After executing mermaid.initialize() or mermaid.init(), remove the before-init-mermaid class to show it.
+  mermaid.initialize({
+    startOnLoad: true,
+    mermaid: {
+      callback:function() {
+        $('.mermaid').removeClass('before-init-mermaid');
+      }
+    }
+  });
+  $('.mermaid').removeClass('before-init-mermaid');
+}
+
+function initMermaidMacro(id) {
+  if (ShowErrorMessageIfIE()) { return false; }
+
+  mermaid.init(undefined, '#' + id);
+}
+
+// Mermaidjs does not support IE. When IE users view the mermaid macro, they will see .mermaid-error.
+function ShowErrorMessageIfIE() {
+  var userAgent = window.navigator.userAgent.toLowerCase();
+  if(userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') != -1) {
+    $('.mermaid').hide();
+    $('.mermaid-error').show();
+    return true;
+  }
+}
--- init_redmica_ui_extension.rb
+++ init_redmica_ui_extension.rb
@@ -19,3 +19,25 @@ require_dependency 'redmica_ui_extension/setting_patch'
 Rails.configuration.to_prepare do
   Setting.include RedmicaUiExtension::SettingPatch
 end
+
+# mermaid macro
+require 'mermaid_macro/hook_listener'
+Redmine::WikiFormatting::Macros.register do
+  desc "Convert the text in the block to a diagram using mermaid.js. Mermaid's Syntax: https://mermaid-js.github.io/mermaid/#/n00b-syntaxReference\n" +
+        "Example:\n\n" +
+        "{{mermaid\n" +
+        "erDiagram\n" +
+        "    CUSTOMER ||--o{ ORDER : places\n" +
+        "    ORDER ||--|{ LINE-ITEM : contains\n" +
+        "    CUSTOMER }|..|{ DELIVERY-ADDRESS : uses\n" +
+        "}}"
+
+  macro :mermaid do |_obj, _args, text|
+    tmp_id = "mermaid-#{SecureRandom.hex(10)}"
+    error_message = t(:error_can_not_execute_macro_html, name: 'mermaid', error: I18n.t('redmica_ui_extension.mermaid_macro.error_mermaid_macro_not_available_in_ie'))
+
+    content_tag(:div, text, class: 'mermaid', id: tmp_id) +
+      content_tag(:div, error_message, class: 'flash error mermaid-error-message') +
+      javascript_tag("initMermaidMacro('#{tmp_id}');")
+  end
+end
