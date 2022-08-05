fefa8c0d0075e3511415facedd287f95fbdc2c09
ishikawa <ishikawa@farend.jp>
2022-08-05 07:24:20 +0000

    Support multiple selectbox

--- init_redmica_ui_extension.rb
+++ init_redmica_ui_extension.rb
@@ -1,39 +1,41 @@
 # frozen_string_literal: true
 
-# Common methods for redmica_ui_extension
-require File.dirname(__FILE__) + '/lib/redmica_ui_extension/setting_patch'
-Setting.include RedmicaUiExtension::SettingPatch
+Rails.configuration.after_initialize do
+  # Common methods for redmica_ui_extension
+  require File.dirname(__FILE__) + '/lib/redmica_ui_extension/setting_patch'
+  Setting.include RedmicaUiExtension::SettingPatch
 
-# searchable_selectbox
-require File.dirname(__FILE__) + '/lib/searchable_selectbox/hook_listener'
-require File.dirname(__FILE__) + '/lib/searchable_selectbox/my_helper_patch'
-MyHelper.include SearchableSelectbox::MyHelperPatch
+  # searchable_selectbox
+  require File.dirname(__FILE__) + '/lib/searchable_selectbox/hook_listener'
+  require File.dirname(__FILE__) + '/lib/searchable_selectbox/my_helper_patch'
+  MyHelper.include SearchableSelectbox::MyHelperPatch
 
-# burndown_chart
-require File.dirname(__FILE__) + '/lib/burndown_chart/hook_listener'
-require File.dirname(__FILE__) + '/lib/burndown_chart/versions_helper_patch'
-VersionsHelper.include BurndownChart::VersionsHelperPatch
+  # burndown_chart
+  require File.dirname(__FILE__) + '/lib/burndown_chart/hook_listener'
+  require File.dirname(__FILE__) + '/lib/burndown_chart/versions_helper_patch'
+  VersionsHelper.include BurndownChart::VersionsHelperPatch
 
-# mermaid macro
-require File.dirname(__FILE__) + '/lib/mermaid_macro/hook_listener'
-Redmine::WikiFormatting::Macros.register do
-  desc "Convert the text in the block to a diagram using mermaid.js. Mermaid's Syntax: https://mermaid-js.github.io/mermaid/#/n00b-syntaxReference\n" +
-        "Example:\n\n" +
-        "{{mermaid\n" +
-        "erDiagram\n" +
-        "    CUSTOMER ||--o{ ORDER : places\n" +
-        "    ORDER ||--|{ LINE-ITEM : contains\n" +
-        "    CUSTOMER }|..|{ DELIVERY-ADDRESS : uses\n" +
-        "}}"
+  # mermaid macro
+  require File.dirname(__FILE__) + '/lib/mermaid_macro/hook_listener'
+  Redmine::WikiFormatting::Macros.register do
+    desc "Convert the text in the block to a diagram using mermaid.js. Mermaid's Syntax: https://mermaid-js.github.io/mermaid/#/n00b-syntaxReference\n" +
+          "Example:\n\n" +
+          "{{mermaid\n" +
+          "erDiagram\n" +
+          "    CUSTOMER ||--o{ ORDER : places\n" +
+          "    ORDER ||--|{ LINE-ITEM : contains\n" +
+          "    CUSTOMER }|..|{ DELIVERY-ADDRESS : uses\n" +
+          "}}"
 
-  macro :mermaid do |_obj, _args, text|
-    tmp_id = "mermaid-#{SecureRandom.hex(10)}"
+    macro :mermaid do |_obj, _args, text|
+      tmp_id = "mermaid-#{SecureRandom.hex(10)}"
 
-    content_tag(:div, text, class: 'mermaid before-init-mermaid', id: tmp_id) +
-      javascript_tag("initMermaidMacro('#{tmp_id}');")
+      content_tag(:div, text, class: 'mermaid before-init-mermaid', id: tmp_id) +
+        javascript_tag("initMermaidMacro('#{tmp_id}');")
+    end
   end
-end
 
-# preview_attachment
-require File.dirname(__FILE__) + '/lib/preview_attachment/hook_listener'
-ApplicationHelper.include PreviewAttachment::ApplicationHelperPatch
+  # preview_attachment
+  require File.dirname(__FILE__) + '/lib/preview_attachment/hook_listener'
+  ApplicationHelper.include PreviewAttachment::ApplicationHelperPatch
+end
--- lib/searchable_selectbox/hook_listener.rb
+++ lib/searchable_selectbox/hook_listener.rb
@@ -8,7 +8,8 @@ module SearchableSelectbox
       stylesheet_link_tag('../searchable_selectbox/stylesheets/select2.min', plugin: 'redmica_ui_extension') +
       stylesheet_link_tag('../searchable_selectbox/stylesheets/searchable_selectbox', plugin: 'redmica_ui_extension') +
       javascript_include_tag('../searchable_selectbox/javascripts/select2.full.min.js', plugin: 'redmica_ui_extension') +
-      javascript_include_tag('../searchable_selectbox/javascripts/searchable_selectbox.js', plugin: 'redmica_ui_extension')
+      javascript_include_tag('../searchable_selectbox/javascripts/searchable_selectbox.js', plugin: 'redmica_ui_extension') +
+      javascript_tag('var enabledMultipleSelectboxSearchable = true;') if Setting.plugin_redmica_ui_extension['searchable_selectbox'].to_h['enabled_multiple_selectbox'] == '1'
     end
   end
 end
