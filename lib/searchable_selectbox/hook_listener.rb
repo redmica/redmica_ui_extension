459c367c729022cc1d905bbca487aac724f0ca98
ishikawa <ishikawa@farend.jp>
2022-08-08 00:47:36 +0000

    Fix view_layouts_base_html_head

--- lib/searchable_selectbox/hook_listener.rb
+++ lib/searchable_selectbox/hook_listener.rb
@@ -5,11 +5,13 @@ module SearchableSelectbox
     def view_layouts_base_html_head(context)
       return '' unless Setting.enabled_redmica_ui_extension_feature?('searchable_selectbox')
 
-      stylesheet_link_tag('../searchable_selectbox/stylesheets/select2.min', plugin: 'redmica_ui_extension') +
-      stylesheet_link_tag('../searchable_selectbox/stylesheets/searchable_selectbox', plugin: 'redmica_ui_extension') +
-      javascript_include_tag('../searchable_selectbox/javascripts/select2.full.min.js', plugin: 'redmica_ui_extension') +
-      javascript_include_tag('../searchable_selectbox/javascripts/searchable_selectbox.js', plugin: 'redmica_ui_extension') +
-      javascript_tag('var enabledMultipleSelectboxSearchable = true;') if Setting.plugin_redmica_ui_extension['searchable_selectbox'].to_h['enabled_multiple_selectbox'] == '1'
+      tags =
+        stylesheet_link_tag('../searchable_selectbox/stylesheets/select2.min', plugin: 'redmica_ui_extension') +
+        stylesheet_link_tag('../searchable_selectbox/stylesheets/searchable_selectbox', plugin: 'redmica_ui_extension') +
+        javascript_include_tag('../searchable_selectbox/javascripts/select2.full.min.js', plugin: 'redmica_ui_extension') +
+        javascript_include_tag('../searchable_selectbox/javascripts/searchable_selectbox.js', plugin: 'redmica_ui_extension')
+      tags += javascript_tag('var enabledMultipleSelectboxSearchable = true;') if Setting.plugin_redmica_ui_extension['searchable_selectbox'].to_h['enabled_multiple_selectbox'] == '1'
+      tags
     end
   end
 end
