a1b8dc7fdf7019daa7b278ce5b9f631f184c9620
ishikawa999 <remember929@gmail.com>
2024-12-27 04:55:01 +0000

    Update mermaid js 11.4.1

--- assets/javascripts/mermaid_macro/mermaid_macro.js
+++ assets/javascripts/mermaid_macro/mermaid_macro.js
@@ -1,36 +1,18 @@
-// The conversion process when drawing the page is performed by initAllMermaidMacro().
-// After that, initMermaidMacro() is executed separately to support mermaid macro added by ajax, etc.
+// Initialize Mermaid globally
+function initAllMermaidMacro() {
+  if (typeof mermaid === 'undefined') return;
+
+  mermaid.initialize({ startOnLoad: true });
+}
+
+// Render a specific Mermaid macro by selector
+// NOTE: If `document.readyState` is not 'complete', no extra conversion is needed because `initAllMermaidMacro` handles it.  
+//       However, if `document.readyState` is not 'complete', it means the element was added dynamically (e.g., via AJAX), so it should be converted separately.
+
+function renderMermaidMacro(selector) {
+  if (typeof mermaid === 'undefined' || document.readyState !== 'complete' || !selector) return;
+
+  mermaid.run({ querySelector: selector });
+}
 
 initAllMermaidMacro();
-
-function initAllMermaidMacro() {
-  if (isMermaidjsIsAvailable() === false) { return false; }
-
-  // Hide the target before the change by before-init-mermaid class until mermaid.initialize() or mermaid.init() is executed.
-  // After executing mermaid.initialize() or mermaid.init(), remove the before-init-mermaid class to show it.
-  mermaid.initialize({
-    startOnLoad: true,
-    mermaid: { callback:function() {
-      $('svg').parents('.mermaid.before-init-mermaid').removeClass('before-init-mermaid');
-    } }
-  });
-}
-
-function initMermaidMacro(id) {
-  if (isMermaidjsIsAvailable() === false) { return false; }
-
-  let target = $('#'+id+'.before-init-mermaid:not(:has(svg))');
-  // If you run init on an element in the display: none state, the diagram will not be generated properly. In that case, do not run init.
-  // Example: This happens when you open the notes tab of the issue page directly.
-  if (target && target.is(':visible')) {
-    mermaid.init(undefined, target);
-  }
-}
-
-function isMermaidjsIsAvailable() {
-  if (typeof mermaid === 'undefined') {
-    $('.mermaid').removeClass('before-init-mermaid');
-    return false;
-  }
-  return true;
-}
