48b59bb3c456695d3cfe23af8d8decee40a702b9
ishikawa999 <remember929@gmail.com>
2025-01-07 07:49:12 +0000

    Change mermaid.js to mermaid.min.js

--- lib/mermaid_macro/hook_listener.rb
+++ lib/mermaid_macro/hook_listener.rb
@@ -4,7 +4,7 @@ module MermaidMacro
   class HookListener < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context)
       stylesheet_link_tag('mermaid_macro/mermaid_macro', plugin: 'redmica_ui_extension') +
-        javascript_include_tag('mermaid_macro/mermaid.js', plugin: 'redmica_ui_extension') +
+        javascript_include_tag('mermaid_macro/mermaid.min.js', plugin: 'redmica_ui_extension') +
         javascript_include_tag('mermaid_macro/mermaid_macro.js', plugin: 'redmica_ui_extension')
     end
   end
