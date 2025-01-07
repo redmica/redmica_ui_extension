05698a7eba6073e4cfb99d4e52fd0ad39a3c3bc9
ishikawa999 <remember929@gmail.com>
2025-01-07 23:45:53 +0000

    Remove unnecessary branch

--- assets/javascripts/mermaid_macro/mermaid_macro.js
+++ assets/javascripts/mermaid_macro/mermaid_macro.js
@@ -9,7 +9,7 @@ function initAllMermaidMacro() {
 // NOTE: If `document.readyState` is not 'complete', no extra conversion is needed because `initAllMermaidMacro` handles it.  
 //       However, if `document.readyState` is not 'complete', it means the element was added dynamically (e.g., via AJAX), so it should be converted separately.
 function renderMermaidMacro(selector) {
-  if (typeof mermaid === 'undefined' || document.readyState !== 'complete' || !selector) return;
+  if (typeof mermaid === 'undefined' || document.readyState !== 'complete') return;
 
   mermaid.run({ querySelector: selector });
 }
