505d47e7e089c475eb9803bf3f00e5a0edc88983
Mizuki Ishikawa <ishikawa@farend.jp>
2025-01-08 08:27:22 +0900

    Remove unnecessary blank
    Co-authored-by: Katsuya HIDAKA <hidakatsuya@users.noreply.github.com>

--- assets/javascripts/mermaid_macro/mermaid_macro.js
+++ assets/javascripts/mermaid_macro/mermaid_macro.js
@@ -8,7 +8,6 @@ function initAllMermaidMacro() {
 // Render a specific Mermaid macro by selector
 // NOTE: If `document.readyState` is not 'complete', no extra conversion is needed because `initAllMermaidMacro` handles it.  
 //       However, if `document.readyState` is not 'complete', it means the element was added dynamically (e.g., via AJAX), so it should be converted separately.
-
 function renderMermaidMacro(selector) {
   if (typeof mermaid === 'undefined' || document.readyState !== 'complete' || !selector) return;
 
